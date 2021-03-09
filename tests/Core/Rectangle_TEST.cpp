fba63d5d479de266cfa5b2e949c2cb3877b0e7a6
MrPickle311 <macmacroll@gmail.com>
2021-03-10 00:46:48 +0100

    TODO: test moving features of rectangles

--- tests/Core/Rectangle_TEST.cpp
+++ tests/Core/Rectangle_TEST.cpp
@@ -17,12 +17,6 @@ protected:
     {
         terminals_.emplace_back(parent,left_corner_pos,right_corner_pos);
     }
-    void deleteRectangle(size_t index)
-    {
-        std::vector<Rectangle> itr{rectangles.begin()};
-        std::advance(itr,index);
-        rectangles.erase(itr);
-    }
     void initEngine(Size terminal_size)
     {
         engine_.start(terminal_size);
@@ -31,13 +25,13 @@ protected:
     {
         return engine_.getRootHook();
     }
-    size_t getConnectedWindowsCount()
+    Rectangle& getRectangle(size_t index)
     {
-    size_t count {0};
-        for(auto& e: rectangles)
-            if(e.isConnected())
-                ++count;
-        return count;
+        return rectangles[index];
+    }
+    Engine& getEngine()
+    {
+        return engine_;
     }
 };
 
@@ -74,12 +68,12 @@ class RectanglePointPointTests:
 
 TEST_P(RectangleSizePointTests,ConstructorAndBorderTest)
 {
-    RectangleSizePointParameters const& param = GetParam(); 
-    this->initEngine(param.terminal_size_});
+    RectangleSizePointParameters const& data_set = GetParam(); 
+    this->initEngine(data_set.terminal_size_});
 
-    if(param.should_except_)
-        EXPECT_THROW(this->addRectangle(param.rec_size_,param.rec_point_,this->getParent()),OutOfTerminal);
-    else EXPECT_NO_THROW(this->addRectangle(param.rec_size_,param.rec_point_,this->getParent()));
+    if(data_set.should_except_)
+        EXPECT_THROW(this->addRectangle(data_set.rec_size_,data_set.rec_point_,this->getEngine(),OutOfTerminal);
+    else EXPECT_NO_THROW(this->addRectangle(data_set.rec_size_,data_set.rec_point_,this->getEngine()));
 }
 
 INSTANTIATE_TYPED_TEST_CASE_P(BasicTests,RectangleSizePointTests,::testing::Values(
@@ -92,7 +86,7 @@ INSTANTIATE_TYPED_TEST_CASE_P(BasicTests,RectangleSizePointTests,::testing::Valu
     RectangleSizePointParameters{ { {200,200} , true } , { Size{30,30} , Point{20,180} } },
     RectangleSizePointParameters{ { {200,200} , true } , { Size{30,30} , Point{180,20} } },
     //single-cell rectangle
-    RectangleSizePointParameters{ { {200,200} , false } , { Size{1,1},Point{199,199} } },
+    RectangleSizePointParameters{ { {200,200} , false } , { Size{1,1},Point{199,199} } }
     //size argument must be >= 1
     RectangleSizePointParameters{ { {200,200} , true } , { Size{0,1},Point{199,199} } },
     RectangleSizePointParameters{ { {200,200} , true } , { Size{1,0},Point{199,199} } },
@@ -100,22 +94,21 @@ INSTANTIATE_TYPED_TEST_CASE_P(BasicTests,RectangleSizePointTests,::testing::Valu
     //point argument must be >= 0
     RectangleSizePointParameters{ { {200,200} , true } , { Size{1,1},Point{-1,199} } },
     RectangleSizePointParameters{ { {200,200} , true } , { Size{1,1},Point{199,-2} } },
-    RectangleSizePointParameters{ { {200,200} , false } , { Size{1,1},Point{199,199} } },
-    RectangleSizePointParameters{ { {200,200} , false } , { Size{1,1},Point{199,199} } }
+    RectangleSizePointParameters{ { {200,200} , true } , { Size{1,1},Point{0,-1} } },
+    RectangleSizePointParameters{ { {200,200} , true } , { Size{1,1},Point{-2,-4} } }
 
 ));
 
-
 TEST_P(RectanglePointPointTests,BorderTest)
 {
-    RectanglePoinPiontParameters const& param = GetParam(); 
-    this->initEngine(param.terminal_size_});
+    RectanglePoinPiontParameters const& data_set = GetParam(); 
+    this->initEngine(data_set.terminal_size_});
 
-    if(param.should_except_)
-        EXPECT_THROW(this->addRectangle(param.rec_point_1,param.rec_point_2,this->getParent()),BadCords);
-    else EXPECT_NO_THROW(this->addRectangle(param.rec_point_1,param.rec_point_2,this->getParent()));
+    if(data_set.should_except_)
+        EXPECT_THROW(this->addRectangle(data_set.rec_point_1,data_set.rec_point_2,this->getEngine()),BadCords);
+    else EXPECT_NO_THROW(this->addRectangle(data_set.rec_point_1,data_set.rec_point_2,this->getEngine()));
 
-}
+};
 
 INSTANTIATE_TYPED_TEST_CASE_P(BasicTests,RectanglePointPointTests,::testing::Values(
     //to fix some bugs Rectangle provides constructor which takes two Points 
@@ -128,8 +121,87 @@ INSTANTIATE_TYPED_TEST_CASE_P(BasicTests,RectanglePointPointTests,::testing::Val
     RectanglePoinPiontParameters{ { {200,200} , true } , { Point{180,180},Point{150,150} } }
 ));
 
+class RectangleNoThrowTests:
+    public TestBase,
+    public testing::TestWithParam<RectanglePoinPiontParameters>
+{};
+
+void testCompositeObjConnections(CompositeObject& obj,size_t rec_count)
+{
+    ASSERT_TRUE(obj.hasChildren());
+    ASSERT_EQ(rec_count,obj.getChildrenCount());
+}
+
+void addSomeRectangles(CompositeObject& target,size_t rec_count,
+                       Point first_point = {50,50},Point second_point = {50,50})
+{
+    for(size_t i{0}; i < rec_count; ++i)
+        this->addRectangle(first_point,second_point,target);
+}
+
+TEST_F(RectangleNoThrowTests,ConnectionsTest)
+{
+    this->initEngine(Size{200,200});
+    size_t rec_count = 10;
+    addSomeRectangles(this->getEngine(),rec_count);
+    
+    testCompositeObjConnections(this->getEngine(),rec_count);
+    for(auto& rec: this->rectangles)
+    {
+        //here base space isn't space of terminal(200x200) but it's 50x50
+        //inside parent rectangle
+        addSomeRectangles(rec,rec_count,Point{0,0},Point{25,25});
+        testCompositeObjConnections(rec,rec_count);
+    }
+};
+
+//very simple testFunction
+void testFunction(Rectangle& rec)
+{
+    //to move rectangle can use Vector type
+    Vector v{1,1}; // 1 down, 1 right
+    rec.move(v);
+}
+
+TEST_F(RectangleNoThrowTests,BrowsingTest)
+{
+    this->initEngine(Size{200,200});
+
+    size_t rec_count = 10;
+    addSomeRectangles(this->getEngine(),rec_count);
+    
+    // library provides special container for Rectangles which is safe and easy in use
+    // (impl: pointers to rectangles)
+    // also provides a bunch of special algorithms for  RectanglesList
+    // availibity of specific algorithms depends on RectanglesList type 
+    // for example, ColorfulRectanglesList provides group algorithms to manipulate 
+    // colors on group or one by one
+    
+    //the base for lists is RectanglesList which for Rectangle type
+    RectanglesList list {this->getEngine().getChildrenList()};
+
+    ASSERT_FALSE(list.empty());
+    
+    //browse is basic algorithm for invoking packed operations on each rectangle
+    //variadic template
+    ASSERT_NO_THROW(browse(list,testFunction));
+    //argument function/functor must be void(Rectangle&) 
+
+    for(auto& rec: this->rectangles)
+        for(size_t i{0}; i < rec_count; ++i)
+        {
+            this->addSomeRectangles(rec,rec_count,Point{15,15},Point{25,25});
+            RectanglesList tempList {rec.getChildrenList(())};
+            ASSERT_FALSE(tempList.empty());
+            EXPECT_NO_THROW(browse(tempList,testFunction);//expect bcs its leaf of the dependency tree
+        }
+};
+
 /*TODO
-    MAKE THE FOLLOWING TESTS 
+    MAKE THE FOLLOWING TESTS ;
+    DEFINE OPERATION PACKAGE AND WRITE TESTS FOR OPERATIONS ON LISTS
+    PARENTS IMPLEMENTS BROWSE OPERATION DIRECTLY : rec.browseChildren(...)
+    BUT FIRST WRITE 
 */
 //Make the other tests
 class MovingTest:
